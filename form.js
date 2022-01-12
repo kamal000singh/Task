8f128f0c1e4f607cb86669451da3553afea1fe61
kamal-singh <kamal000rawat@gmail.com>
2022-01-12 16:40:14 +0530

    add comment

--- form.js
+++ form.js
@@ -1,3 +1,4 @@
+//gender function is used for get the value from radio button
 function gender() {
   let res = document.getElementsByName("gender");
   for (let i = 0; i < res.length; i++) {
@@ -8,24 +9,35 @@ function gender() {
   return res;
 }
 
-document.getElementById("submit").onclick = function () {
+//submitData() function execute when submit button clicked
+document.getElementById("submit").onclick = function submitData() {
+  //diplay table after clicked on submit button
   document.getElementById("table").style.display = "block";
+
+  //get name value by id from text input field
   document.getElementById("dname").innerHTML =
     document.getElementById("name").value;
 
+  //get email value by id from email input field
   document.getElementById("demail").innerHTML =
     document.getElementById("email").value;
 
+  //get contact value by id from number input field
   document.getElementById("dcontact").innerHTML =
     document.getElementById("contact").value;
 
+  //get dob value by id from date input field
   document.getElementById("ddob").innerHTML =
     document.getElementById("dob").value;
 
+  //get gender value by id from radio input field
   document.getElementById("dgender").innerHTML = gender();
 
+  //get hq value by id from option select field
   document.getElementById("dhq").innerHTML =
     document.getElementById("hq").value;
+
+  //get resume value by id from file input field
   document.getElementById("dresume").innerHTML =
     document.getElementById("resume").value;
 };
